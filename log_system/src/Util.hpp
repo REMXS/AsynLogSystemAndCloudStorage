b677921ea39c2ba6fc7030f998d721188a0d06f1
zuodui_in_wsl2 <zuodui13@gmail.com>
2025-11-12 15:50:01 +0800

    完成了Util文件的编写，并进行了基本测试

--- log_system/src/Util.hpp
+++ log_system/src/Util.hpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <sstream>
+#include <memory>
 
 #include <jsoncpp/json/json.h>
 
@@ -58,7 +60,7 @@ std::string folderPath(const std::string&file_name)
 }
 
 //递归创建目录
-void createDictory(const std::string&path_name)
+void createDirectory(const std::string&path_name)
 {
     if(path_name.empty())
     {
@@ -167,6 +169,164 @@ bool getFileContent(std::string&content,const std::string&file_name)
     
 } // namespace File
 
+namespace JsonUtil
+{
 
+//json对象序列化成字符串
+bool serialize(const Json::Value& val,std::string& str, bool pretty=false)
+{
+    /*
+    建造者->构建配置（这里使用默认配置）->构建具体的工作流(StreamWriter),而工作流的具体流程是不变的(从json对象转换为字符串)
+    但是配置项是有剧烈变化的(StreamWriterBuilder设置的行前缩进，启动注释之类的功能)
+    */
+    Json::StreamWriterBuilder swb;
+    if(!pretty)
+    {
+        swb["indentation"] = "";
+        swb["commentStyle"] = "None";
+    }
+
+    std::unique_ptr<Json::StreamWriter> cwb(swb.newStreamWriter());
+    std::stringstream oss;
+    if(cwb->write(val,&oss)!=0)
+    {
+        std::cerr<<__FUNCTION__<<":"<<__LINE__<<" serialize failed"<<std::endl;
+        return false;
+    }
+    str=oss.str();
+    return true;
+}
+
+//字符串序列化为json对象
+bool parse(const std::string& str,Json::Value& val)
+{
+    Json::CharReaderBuilder crb;
+    std::unique_ptr<Json::CharReader>ccr(crb.newCharReader());
+
+    std::string err;
+    if(ccr->parse(str.c_str(),str.c_str()+str.size(),&val,&err)==false)
+    {
+        std::cerr<<__FUNCTION__<<":"<<__LINE__<<" parse failed -> "<<err<<std::endl;
+        return false;
+    }
+    return true;
+}
+
+
+/*
+因为这种Meyers 单例实现难以测试，所以把这个类的职责 “1.加载配置 2.保证唯一性”
+中的第二项由这个类保证变为由应用程序保证，实现这两个职责的解耦。
+struct JsonData
+{
+    static JsonData& getJsonData()
+    {
+        static JsonData instance;
+        return instance;
+    }
+    JsonData(const JsonData&)=delete;
+    JsonData& operator=(const JsonData&)=delete;
+
+    size_t buffer_size_; //缓冲区的初始大小
+    size_t threshold_; //缓冲区的倍数扩容阈值
+    size_t linear_growth_; //缓冲区的线性扩容的扩容容量
+    size_t flush_log_; //控制日志同步到磁盘的时机，默认为0，1为调用fflush，2为调用fsync
+    std::string backup_addr_; //备份的服务器的ip地址
+    uint16_t backup_port_; //备份服务器的端口号
+    size_t thread_count_; //日志系统内部线程池的数量
+
+private:
+    // 加载默认参数的辅助函数
+    void loadDefaults() {
+        std::cout << "WARNING: config.conf not found or invalid. Using default settings." << std::endl;
+        buffer_size_ = 4 * 1024 * 1024; // 4MB
+        threshold_ = 1024;              // 1KB
+        linear_growth_ = 1024 * 1024;   // 1MB
+        flush_log_ = 1;                 // fflush
+        backup_addr_ = "127.0.0.1";
+        backup_port_ = 8080;
+        thread_count_ = 1;
+    }
+
+    JsonData()
+    {
+        std::string content;
+        //获取文件失败，使用默认配置
+        if(File::getFileContent(content,"log_config.conf")==false)
+        {
+            loadDefaults();
+            return;
+        }
+
+        Json::Value root;
+        //解析文件失败，使用默认配置
+        if(parse(content,root)==false)
+        {
+            loadDefaults();
+            return;
+        }
+
+        buffer_size_=root["buffer_size"].asUInt64();
+        threshold_=root["threshold"].asUInt64();
+        linear_growth_=root["linear_growth"].asUInt64();
+        flush_log_=root["flush_log"].asUInt64();
+        backup_addr_=root["backup_addr"].asString();
+        backup_port_=root["backup_port"].asUInt();
+        thread_count_=root["thread_count"].asUInt64();
+    }
+};
+*/
+
+struct JsonData
+{
+    JsonData(const JsonData&)=delete;
+    JsonData& operator=(const JsonData&)=delete;
+
+    size_t buffer_size_; //缓冲区的初始大小
+    size_t threshold_; //缓冲区的倍数扩容阈值
+    size_t linear_growth_; //缓冲区的线性扩容的扩容容量
+    size_t flush_log_; //控制日志同步到磁盘的时机，默认为0，1为调用fflush，2为调用fsync
+    std::string backup_addr_; //备份的服务器的ip地址
+    uint16_t backup_port_; //备份服务器的端口号
+    size_t thread_count_; //日志系统内部线程池的数量
+
+    JsonData()
+        :buffer_size_ ( 4 * 1024 * 1024) // 4MB
+        ,threshold_  (1024)              // 1KB
+        ,linear_growth_ ( 1024 * 1024)   // 1MB
+        ,flush_log_ (1)                 // fflush
+        ,backup_addr_ ( "127.0.0.1")
+        ,backup_port_ (8080)
+        ,thread_count_ (1)
+    {}
+
+    void loadConfig(const std::string&file_path)
+    {
+        std::string content;
+        //获取文件失败，使用默认配置
+        if(File::getFileContent(content,file_path)==false)
+        {
+            std::cout <<__FUNCTION__<<":"<<__LINE__<<"->WARNING: config.conf not found or invalid. Using default settings." << std::endl;    
+            return;
+        }
+
+        Json::Value root;
+        //解析文件失败，使用默认配置
+        if(parse(content,root)==false)
+        {
+            std::cout <<__FUNCTION__<<":"<<__LINE__<<"->WARNING: parse file failed. Using default settings." << std::endl;    
+            return;
+        }
+
+        buffer_size_=root["buffer_size"].asUInt64();
+        threshold_=root["threshold"].asUInt64();
+        linear_growth_=root["linear_growth"].asUInt64();
+        flush_log_=root["flush_log"].asUInt64();
+        backup_addr_=root["backup_addr"].asString();
+        backup_port_=root["backup_port"].asUInt();
+        thread_count_=root["thread_count"].asUInt64();
+    }
+};
+
+} //namespace JsonUtil
 
 } // namespace asynclog::Util
--- log_system/test/test_Util.h
+++ log_system/test/test_Util.h
@@ -5,26 +5,38 @@
 #include <sys/types.h>
 #include <filesystem>
 #include <chrono>
+#include <vector>
+
+namespace fs = std::filesystem;
 
 class UtilTest: public ::testing::Test
 {
 protected:
-    inline static std::string file_name;
+    inline static fs::path test_dir;
+    inline static fs::path test_file;
     inline static std::string file_content;
 
     static void SetUpTestSuite()
     {
-        file_name="./get_file.txt";
+        test_dir = fs::temp_directory_path()/"UtilTestDir";
+        test_file = test_dir / "get_file.txt";
         file_content="hello world, this is a test";
 
-        std::ofstream ofs(file_name);
+        //确保test_dir为新的
+        fs::remove_all(test_dir);
+        fs::create_directories(test_dir);
+
+        //向测试文件中写入数据
+        std::ofstream ofs(test_file);
         ASSERT_TRUE(ofs.is_open());
         ofs<<file_content;
         ofs.close();
     }
+
     static void TearDownTestSuite()
     {
-        std::filesystem::remove(file_name);
+        //递归删除所有测试文件夹中的内容和测试文件夹
+        std::filesystem::remove_all(test_dir);
     }
 };
 
@@ -35,47 +47,42 @@ TEST_F(UtilTest,date_test)
     ASSERT_NEAR(time_,std_time_,1);
 }
 
-TEST_F(UtilTest,create_dictory_test)
+TEST_F(UtilTest,create_directory_test)
 {
     using namespace asynclog::Util::File;
-    std::string path_name1="./test1/test01";
-    std::string path_name2="./test1/test02";
-    std::string path_name3="./test1/../test1";
-    std::string path_name4="./test1/..";
-    std::string path_name5="./../test/test2";
-    std::string_view path_name2_sv(path_name2);
+    fs::path path1 = test_dir / "test1" / "test01";
+    fs::path path2 = test_dir / "test1" / "test02";
+    fs::path path3 = test_dir / "test1" / ".." / "test1"; // 等价于 test_dir / "test1"
+    fs::path path4 = test_dir / "test_already_exists";
 
-    ASSERT_FALSE(exists(path_name1));
-    ASSERT_FALSE(exists(path_name2));
+    ASSERT_FALSE(exists(path1.string()));
+    ASSERT_FALSE(exists(path2.string()));
 
-    //创建目录
-    createDictory(path_name1);
-    createDictory(path_name2);
-    createDictory(path_name3);
-    createDictory(path_name5);
-    
-    ASSERT_TRUE(exists(path_name1));
-    ASSERT_TRUE(exists(path_name2));
-    ASSERT_TRUE(exists(path_name3));
-    ASSERT_TRUE(exists(path_name2_sv));
-    
-    rmdir(path_name1.c_str());
-    rmdir(path_name2.c_str());
-    rmdir(path_name5.c_str());
-    rmdir(path_name3.c_str());
+    createDirectory(path1.string());
+    createDirectory(path2.string());
+    createDirectory(path3.string());
+
+    fs::create_directory(path4);
+    ASSERT_TRUE(exists(path4.string()));
+    createDirectory(path4.string());
+
+    // 结果验证
+    ASSERT_TRUE(exists(path1.string()));
+    ASSERT_TRUE(exists(path2.string()));
+    ASSERT_TRUE(exists(path3.string()));
+    ASSERT_TRUE(exists(path4.string()));
+
+    //测试string_view 重载
+    std::string temp_path1=path1.string();
+    std::string_view path1_sv(temp_path1);
+    ASSERT_TRUE(exists(path1_sv));
 }
 
 TEST_F(UtilTest,get_file_size_test)
 {
     using namespace asynclog::Util::File;
 
-    std::ifstream ifs(file_name,std::ios::ate);
-    ASSERT_TRUE(ifs.is_open());
-
-    auto file_len=ifs.tellg();
-    ASSERT_TRUE(file_len!=-1);
-
-    ASSERT_EQ(file_len,fileSize(file_name));
+    ASSERT_EQ(file_content.size(),fileSize(test_file.string()));
 }
 
 TEST_F(UtilTest,get_file_content_test)
@@ -83,8 +90,96 @@ TEST_F(UtilTest,get_file_content_test)
     using namespace asynclog::Util::File;
     std::string content;
     
-    bool is_success=getFileContent(content,file_name);
+    bool is_success=getFileContent(content,test_file.string());
     ASSERT_TRUE(is_success);
 
     ASSERT_EQ(content,file_content);
+}
+
+TEST_F(UtilTest,json_test)
+{
+    using namespace asynclog::Util;
+    std::vector<std::pair<Json::Value,std::string>>vals(5); //json对象和其对应的字符串
+    vals[0].first["name"] = "Alice";
+    vals[0].first["age"] = 30;
+    vals[0].first["isStudent"] = false;
+    vals[0].second=R"({"name":"Alice","age":30,"isStudent":false})";
+
+    vals[1].first.append("apple");
+    vals[1].first.append("banana");
+    vals[1].first.append("cherry");
+    vals[1].second=R"(["apple","banana","cherry"])";
+
+    vals[2].first["user"]["id"] = 123;
+    vals[2].first["user"]["name"] = "Bob";
+    vals[2].first["scores"].append(85);
+    vals[2].first["scores"].append(92);
+    vals[2].second=R"({"user":{"id":123,"name":"Bob"},"scores":[85,92]})";
+
+    vals[3].first=Json::Value(Json::objectValue);
+    vals[3].second=R"({})";
+
+
+    vals[4].first["key1"] = "value";
+    vals[4].first["key2"] = Json::nullValue;
+    vals[4].second=R"({"key1":"value","key2":null})";
+
+    for(auto&[json_val,str]:vals)
+    {
+        Json::Value ret_json_val;
+        //验证从字符串转化为json对象
+        JsonUtil::parse(str,ret_json_val);
+        ASSERT_EQ(ret_json_val,json_val);
+
+        //验证从json转换为字符串
+        //因为直接比较字符串很困难，所以先把json转化为字符串，然后再将字符串转化为json来验证
+        std::string ret_str;
+        Json::Value tem_json;
+        JsonUtil::serialize(json_val,ret_str);
+        JsonUtil::parse(ret_str,tem_json);
+        ASSERT_EQ(tem_json,json_val);
+    }
+}
+
+TEST_F(UtilTest,config_test_default)
+{
+    using namespace asynclog::Util;
+
+    std::string config_content=R"({
+    "buffer_size": 10000000,       
+    "threshold": 10000000000,      
+    "linear_growth" : 10000000,
+    "flush_log" : 2,
+    "backup_addr" : "47.116.74.254",
+    "backup_port" : 8080,
+    "thread_count" : 3
+})";
+    fs::path conf_path="./log_config.conf";
+    {
+        std::ofstream ofs(conf_path);
+        ASSERT_TRUE(ofs.is_open());
+        ofs.write(config_content.c_str(),config_content.size());
+        ofs.close();
+    }
+
+    JsonUtil::JsonData default_config;
+    JsonUtil::JsonData j_config;
+    j_config.loadConfig(conf_path.string());
+    default_config.loadConfig("sfsdf");
+
+    ASSERT_EQ(j_config.buffer_size_,10000000);
+    ASSERT_EQ(j_config.threshold_,10000000000);
+    ASSERT_EQ(j_config.linear_growth_,10000000);
+    ASSERT_EQ(j_config.flush_log_,2);
+    ASSERT_EQ(j_config.backup_addr_,"47.116.74.254");
+    ASSERT_EQ(j_config.backup_port_,8080);
+    ASSERT_EQ(j_config.thread_count_,3);
+
+    ASSERT_EQ(default_config.buffer_size_,4 * 1024 * 1024);
+    ASSERT_EQ(default_config.threshold_,1024);
+    ASSERT_EQ(default_config.linear_growth_,1024 * 1024);
+    ASSERT_EQ(default_config.flush_log_,1);
+    ASSERT_EQ(default_config.backup_addr_,"127.0.0.1");
+    ASSERT_EQ(default_config.backup_port_,8080);
+    ASSERT_EQ(default_config.thread_count_,1);
 }
